606f0164adb4042293ca2edc54ac730723033682
oscaroceguera <oscaroceguera@gmail.com>
2017-10-30 10:12:58 -0600

    Config router for schema and schema list

---
+++ src/containers/Schema/SchemaSingle/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class SchemaContainer extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Schema container</h1>
+        <div>
+          <h2>Modules</h2>
+          <h2>Items</h2>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SchemaContainer
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,11 @@
 import App from './App'
+import SchemaListContainer from './Schema/SchemaList'
+import SchemaContainer from './Schema/SchemaSingle'
+import NoMatch from './Screens/NoMatch'
 
 export {
-  App
+  App,
+  SchemaListContainer,
+  SchemaContainer,
+  NoMatch
 }
