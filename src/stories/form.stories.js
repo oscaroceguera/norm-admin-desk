c6afb2c9331f8234471cf952a0ef118d1eff98ce
oscaroceguera <oscaroceguera@gmail.com>
2017-11-06 03:38:34 -0600

    #20 Selects

--- src/stories/form.stories.js
+++ src/stories/form.stories.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, text, select } from '@storybook/addon-knobs'
-import { TextField } from '../components'
+import { TextField, SelectField } from '../components'
+import './styles.css'
 
-const SELECT_OPTIONS = {
+const { label, options, defaultValue } = {
   label: 'Widths',
   options: {
     Small: 'Small',
@@ -14,17 +15,34 @@ const SELECT_OPTIONS = {
   defaultValue: 'Medium'
 }
 
+const ITEMS = [
+  { id: '9de64059-d62f-4102-a61b-c4022c43d8d9', desc: 'value1' },
+  { id: '11176329-4382-4eae-bd81-de8f886e04da', desc: 'value2' },
+  { id: '3f658795-048f-494b-9a49-ae6626c4f7cc', desc: 'value3' },
+  { id: 'bcac5fc8-badf-41bb-ad08-3fbcc10492b2', desc: 'value4' },
+  { id: 'c33b8842-e6d0-40ba-8b29-89bf851031a4', desc: 'value5' }
+]
+
 const stories = storiesOf('Form', module)
 stories.addDecorator(withKnobs)
 
 stories
   .add('TextField', () => {
-    const { label, options, defaultValue } = SELECT_OPTIONS
     return (
       <TextField
         width={select(label, options, defaultValue)}
         placeholder={text('Placeholder')}
         title={text('Title', 'Email')}
+      />
+    )
+  })
+
+  .add('SelectField', () => {
+    return (
+      <SelectField
+        items={ITEMS}
+        title={text('Title', 'Seleccione una opción')}
+        width={select(label, options, defaultValue)}
       />
     )
   })
