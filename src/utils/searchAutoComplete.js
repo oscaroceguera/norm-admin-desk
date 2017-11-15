ece96e3b8b530fd8dd61121684443a5bef104a75
oscaroceguera <oscaroceguera@gmail.com>
2017-11-15 03:32:26 -0600

    #20 - UI Autocomplete

--- src/utils/searchAutoComplete.js
+++ src/utils/searchAutoComplete.js
@@ -1,25 +1,11 @@
+import {deburr} from 'lodash/string'
+
 const searchData = (data, value) => {
-  class SearchData {
-    constructor (data, value) {
-      this.data = data
-      this.value = value
-    }
-
-    filter () {
-      const {data, value} = this
-      return data.filter(x => x.desc.toLowerCase().indexOf(value.toLowerCase()) > -1)
-    }
-
-    get getSource () {
-      if (this.value.length < 1) {
-        return []
-      }
-      return this.filter()
-    }
+  if (value.length === 0) {
+    return []
   }
-
-  const searchData = new SearchData(data, value)
-  return searchData.getSource
+  const lowerValue = value.toLowerCase()
+  return data.filter(x => x.desc.toLowerCase().includes(lowerValue))
 }
 
 export default searchData
