f765bdf19e00184a61b10c216414b73774601c2d
oscaroceguera <oscaroceguera@gmail.com>
2017-12-17 22:15:11 -0600

    remove exact in routes

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -14,7 +14,7 @@ class App extends Component {
         <Router>
           <Switch>
             <Route exact path='/' component={SchemaListContainer} />
-            <Route exact path='/schema' component={SchemaContainer} />
+            <Route path='/schema' component={SchemaContainer} />
             <Route component={NoMatch} />
           </Switch>
         </Router>
