644c40bad463443088f6a414d163dca2bf8d7bfd
oscaroceguera <oscaroceguera@gmail.com>
2018-04-28 00:59:00 -0500

    test uri

--- src/api/index.js
+++ src/api/index.js
@@ -1,30 +1,26 @@
 import axios from 'axios'
 
-let API_HOST = 'http://localhost:5000/api'
-
-if (process.env.NODE_ENV === 'production') {
-  API_HOST = 'https://blooming-ravine-18324.herokuapp.com/api'
-}
+const apiHost = process.env.API_HOST || 'http://localhost:5000/api'
 
 export const api = {
   get: (url) => {
     return axios
-      .get(`${API_HOST}/${url}`)
+      .get(`${apiHost}/${url}`)
       .then(res => res.data)
   },
   post: (url, data) => {
     return axios
-      .post(`${API_HOST}/${url}`, data)
+      .post(`${apiHost}/${url}`, data)
       .then(res => res.data)
   },
   put: (url, data) => {
     return axios
-      .patch(`${API_HOST}/${url}`, data)
+      .patch(`${apiHost}/${url}`, data)
       .then(res => res.data)
   },
   delete: (url) => {
     return axios
-      .delete(`${API_HOST}/${url}`)
+      .delete(`${apiHost}/${url}`)
       .then(res => res.data)
   }
 }
